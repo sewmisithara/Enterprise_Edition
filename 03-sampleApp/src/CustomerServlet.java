1897e906c5bc49c91c87210a4214c43f1e2a2b35
sewmisithara <sewmisithara23520@gmail.com>
2023-08-20 13:00:52 +0530

    03 sample app

--- 03-sampleApp/src/CustomerServlet.java
+++ 03-sampleApp/src/CustomerServlet.java
@@ -4,16 +4,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.Writer;
+
 
 @WebServlet(urlPatterns = "/customer")
-public class MyClass  extends HttpServlet {
-
+public class CustomerServlet  extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-      PrintWriter writer=resp.getWriter();
-
-        writer.write("responce Genarated");
+        System.out.println("customer save");
     }
 }
